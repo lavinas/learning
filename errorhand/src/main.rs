e56b52f6d13e04eda36ab7006d7a7edcc8206a7d
lavinas <lavinas@me.com>
2024-11-18 17:24:03 -0300

    errhand

--- errorhand/src/main.rs
+++ errorhand/src/main.rs
@@ -1,21 +1,34 @@
-use std::fs::File;
+use std::{fs::File, io::Read};
 
 fn main() {
-     let res = read_username_from_file("hello.txt".to_string());
-        match res {
-            Ok(s) => println!("The content of the file is: {}", s),
-            Err(e) => println!("Error: {}", e),
-        }
+    let res = read_username_from_file("hello.txt".to_string());
+    match res {
+        Ok(s) => println!("The content of the file is: {}", s),
+        Err(e) => println!("Error: {}", e),
+    }
+    let res = read_username_from_file_2("hello.txt".to_string());
+    match res {
+        Ok(s) => println!("The content of the file is: {}", s),
+        Err(e) => println!("Error: {}", e),
+    }
 }
 
-fn read_username_from_file(fileName: String) -> Result<String, std::io::Error> {
-    let mut f = match File::open(fileName) {
+fn read_username_from_file(file_name: String) -> Result<String, std::io::Error> {
+    let mut username_file = match File::open(file_name) {
         Ok(file) => file,
         Err(e) => return Err(e),
     };
-    let mut s = String::new();
-    match f.read_to_string(&mut s) {
-        Ok(_) => Ok(s),
+    
+    let mut username = String::new();
+   
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
         Err(e) => Err(e),
     }
+}
+
+fn read_username_from_file_2(file_name: String) -> Result<String, std::io::Error> {
+    let mut username = String::new();
+    File::open(file_name)?.read_to_string(&mut username)?;
+    Ok(username)
 }
