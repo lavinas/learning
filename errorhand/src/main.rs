81df8ba5a66783632f52b4975533a743bd687561
lavinas <lavinas@me.com>
2024-11-18 13:35:15 -0300

    error

--- errorhand/src/main.rs
+++ errorhand/src/main.rs
@@ -1,20 +1,21 @@
 use std::fs::File;
-use std::io::ErrorKind;
-
 
 fn main() {
-    let _greeting_file = match File::open("hello.txt") {
+     let res = read_username_from_file("hello.txt".to_string());
+        match res {
+            Ok(s) => println!("The content of the file is: {}", s),
+            Err(e) => println!("Error: {}", e),
+        }
+}
+
+fn read_username_from_file(fileName: String) -> Result<String, std::io::Error> {
+    let mut f = match File::open(fileName) {
         Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(file) => panic!("Here"),
-                Err(error) => {
-                    panic!("Problem creating the file: {:?}", error)
-                },
-            },
-            _ => {
-                panic!("Problem opening the file: {:?}", error)
-            },
-        },
+        Err(e) => return Err(e),
     };
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
