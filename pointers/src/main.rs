3d722360b69ddaffc425caeba3241946afe40956
lavinas <lavinas@me.com>
2024-11-30 15:48:41 -0300

    pointers

--- pointers/src/main.rs
+++ pointers/src/main.rs
@@ -41,6 +41,7 @@ impl Drop for CustomSmartPointer {
 }
 
 use std::rc::Rc;
+#[derive(Debug)]
 enum List {
     Cons(i32, Rc<List>),
     Nil,
@@ -81,10 +82,10 @@ fn main() {
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
     println!("count after creating a = {}", Rc::strong_count(&a));
     let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
+    println!("count after creating b{:?} = {}", b, Rc::strong_count(&a));
     {
         let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
+        println!("count after creating c{:?} = {}", c, Rc::strong_count(&a));
     }
     println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 
