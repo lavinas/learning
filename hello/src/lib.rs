e6eab000869a65f8c7377db4184568d79b31622d
lavinas <lavinas@me.com>
2024-12-16 19:38:37 -0300

    hello x

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,7 +1,55 @@
-pub struct ThreadPool;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread};
+
+struct Job;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
 
 impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
+        assert!(size > 0);
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
+
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+        ThreadPool { workers, sender }
+    }   
+    /// Execute the function f on a thread in the pool.
+    /// 
+    /// # Panics
+    /// 
+    /// The `execute` function will panic if the closure `f` panics.
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<Arc<Mutex<mpsc::Receiver<Job>>>>,
+}
+
+impl Worker {
+    /// Create a new Worker`.
+    fn new(id: usize, receiver:Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| receiver);
+        Worker { id, thread }
     }
 }
